271d96c2e75c944d5ffb5e513782ee258e6e4524
Nadeesha Fernando <itsme@nadeeshafdo.dev>
2026-02-28 20:43:20 +0530

    chore: indentation corrections

--- comp.py
+++ comp.py
@@ -10,29 +10,29 @@ def encode_file_to_image(input_filepath, output_image_path):
             raw_data = f.read()
     except FileNotFoundError:
         raise FileNotFoundError(f"The input file '{input_filepath}' does not exist.")
-    
+
     # Compress the payload mercilessly
     compressed_data = lzma.compress(raw_data, preset=lzma.PRESET_EXTREME)
     compressed_size = len(compressed_data)
-    
+
     # Metadata Header: Magic bytes 'B2Z' + 64-bit unsigned integer
     magic_bytes = b'B2Z'
     size_bytes = struct.pack('>Q', compressed_size)
     header = magic_bytes + size_bytes
-    
+
     full_data = bytearray(header + compressed_data)
-    
+
     total_bytes = len(full_data)
     total_pixels = math.ceil(total_bytes / 3)
-    
+
     width = math.ceil(math.sqrt(total_pixels))
     height = width 
-    
+
     required_bytes = width * height * 3
     padding_needed = required_bytes - total_bytes
     full_data.extend(b'\x00' * padding_needed)
-    
+
     img = Image.frombytes('RGB', (width, height), bytes(full_data))
     img.save(output_image_path, 'PNG')
-    
+
     return len(raw_data), compressed_size, width, height
